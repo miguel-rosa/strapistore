a590c75bedb0abb8567fc4fbdac60612c38fbdec
Miguel Rosa <miguelg1@motorola.com>
2021-01-10 16:31:17 -0300

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '5aade101815787db0020c389bf5c261f'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '703274c2-9d85-4975-b8e7-f820b1fde1c2'
+};
