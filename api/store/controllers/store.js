ce1bf10dd50d67475b7c0577ada3a26982c2d771
Miguel Rosa <miguelg1@motorola.com>
2021-01-11 19:01:25 -0300

    fix slug controller

--- api/store/controllers/store.js
+++ api/store/controllers/store.js
@@ -4,5 +4,23 @@
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
+const { sanitizeEntity } = require('strapi-utils');
 
-module.exports = {};
+module.exports = {
+  async findOne(ctx) {
+    //check if the params id is an id or a slug
+    const {id} = ctx.params;
+
+    // if you use SQL database
+    // we check if the id is a valid number
+    if (parseInt(id) == id) {
+      const entity = await strapi.services.store.findOne(ctx.params);
+      return sanitizeEntity(entity, { model: strapi.models.article });
+    }
+
+    // findOne function works only with IDs
+    // so we find all and get first entry by using slug
+    const [entity] = await strapi.services.store.find({slug: id});
+    return sanitizeEntity(entity, { model: strapi.models.store });
+  }
+};
